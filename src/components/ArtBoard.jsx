8032a2a8a4e1e11e45c3535fd71f36d0965c9e4f
tapader13 <minhajtapader0@gmail.com>
2025-03-30 17:48:24 -0400

    react to pdf convert

---
+++ src/components/ArtBoard.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+
+const ArtBoard = ({ ref }) => {
+  return (
+    <div
+      ref={ref}
+      className='h-[1123px] w-[794px] p-16 bg-white shadow-lg mx-auto'
+    >
+      <h1 className='font-bold text-left text-2xl text-red-400'>Art Board</h1>
+      <h1 className='font-bold text-right text-2xl text-red-400'>Art Board</h1>
+      <p className='text-justify text-gray-600 mt-5'>
+        derit deleniti animi ducimus quos. Illo, iure quis quidem labore neque
+        quaerat fuga repellat. Voluptatibus, aperiam ducimus, sunt distinctio
+        sapiente veritatis et ullam porro molestiae eius dolorum doloribus
+        placeat ipsam quaerat doloremque illum aspernatur alias debitis, aut nam
+        laborum temporibus eum. Itaque, rem quasi adipisci explicabo vitae earum
+        neque mollitia aperiam ipsa, maxime modi minus fugiat facere dolor
+        deserunt id debitis sunt quibusdam! Consectetur corporis dolorum porro
+        ut laborum quam ipsum, labore debitis placeat unde nobis, incidunt
+        eligendi voluptates mollitia suscipit et rerum commodi assumenda
+        doloremque exercitationem quod sequi hic nemo voluptatibus! Numquam
+        fugiat nulla ex. Repudiandae quas perferendis veritatis, vitae aliquam
+        minus, reiciendis laboriosam corporis, provident voluptatibus ut
+        incidunt quis deserunt odio. Eius eligendi sit ullam qui! Ad expedita
+        tempora ipsam nulla? Quibusdam, maxime ut? Quod, labore nihil quam fugit
+        quibusdam fuga. Illo corporis dignissimos, eaque quos magnam natus quo
+        placeat quibusdam minima totam animi voluptate corrupti cupiditate odio
+        fugit fuga numquam vel sint reprehenderit dolores. Recusandae molestias
+        ducimus eligendi, officiis tempora nesciunt voluptate maxime deserunt ea
+        quis labore et excepturi dolore placeat! Non fugiat impedit corrupti ut
+        repellat optio facere cupiditate voluptate dignissimos laborum eius
+        autem quis ex suscipit, earum sint odio eaque iste. Nesciunt tenetur,
+        repellendus earum dolor obcaecati nobis officia laborum autem, ab quasi
+        dolore consequatur beatae quae perspiciatis. Voluptate, esse laboriosam
+        similique veniam velit facere eveniet aspernatur error aperiam ullam,
+        natus, iusto nostrum debitis consequuntur nam dignissimos fuga. Delectus
+        iusto repellat facilis tempora obcaecati incidunt, quis iure recusandae
+        vel voluptatem architecto molestiae dolorem nesciunt ex, odit sapiente
+        at pariatur? Laborum obcaecati minima at! Unde odit eligendi distinctio
+        magni beatae illum voluptates alias quae! Nam dolore obcaecati, a qui
+        quasi ex consequatur reiciendis ducimus blanditiis doloribus ratione est
+        mollitia ea debitis omnis, eos deleniti accusamus quis inventore? Saepe
+        accusamus ut mollitia illum eveniet expedita harum, molestiae quis sequi
+        magnam, a ipsa aperiam assumenda quod cum numquam ducimus labore quo
+        voluptatum rerum eaque id qui eius? Vitae nostrum doloribus corrupti
+        eveniet, nam nihil consectetur illum dolores quas, in quae temporibus,
+        iure est itaque aperiam doloremque! Quo totam officia illum, cum quod
+        blanditiis obcaecati provident dignissimos nesciunt iusto molestias.
+        Eaque, sed, eos, quisquam aspernatur temporibus architecto eveniet
+        obcaecati doloremque nobis dignissimos quam illum quas. Libero eum
+        tenetur, iure ipsum consectetur amet aperiam pariatur odit recusandae
+        enim quis ex, sapiente molestiae numquam commodi quisquam dolores
+        officia! Repellat distinctio modi ex quibusdam accusantium culpa,
+        expedita necessitatibus quia doloremque tempore deserunt maiores officia
+        a consequatur dolorem voluptatum natus accusamus magni aut impedit sit
+        labore dicta. Quae animi culpa praesentium, repudiandae tenetur, nihil
+        illo dicta sapiente itaque repellendus odio rem officia dignissimos
+        accusantium asperiores magni vitae ratione sit sed voluptates? Pr
+        voluptatum beatae fuga eius. Temporibus, sit placeat.
+      </p>
+    </div>
+  );
+};
+
+export default ArtBoard;
--- src/components/tamplete.jsx
+++ src/components/tamplete.jsx
@@ -1,12 +1,15 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Tamplete = ({ item }) => {
   return (
     <div>
-      <div className='p-5 cursor-pointer bg-[rgba(227,233,245,.4)] hover:bg-[#E0EEE9]'>
-        <img className='h-96' src={item.img} alt={item.title} />
-        <p className='text-center mt-5 font-bold text-xl '>{item.title}</p>
-      </div>
+      <Link href={`/tamplete/${item.title}`}>
+        <div className='p-5 cursor-pointer bg-[rgba(227,233,245,.4)] hover:bg-[#E0EEE9]'>
+          <img className='h-96' src={item.img} alt={item.title} />
+          <p className='text-center mt-5 font-bold text-xl '>{item.title}</p>
+        </div>
+      </Link>
     </div>
   );
 };
