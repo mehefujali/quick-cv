0a6fe07577a8d18efbe5ed4a41b34ea4091fa15e
tapader13 <minhajtapader0@gmail.com>
2025-03-30 01:59:46 -0400

    setup material tailwind

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+'use client';
+import { Button } from '@material-tailwind/react';
+import React from 'react';
 
 export default function Home() {
   return (
     <div>
       Hello World
+      <Button>Test</Button>
     </div>
-  )
+  );
 }
